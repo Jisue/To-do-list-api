e062826f649929e49d3c7565548a56a10b6a0ada
Jisue <49144667+Jisue@users.noreply.github.com>
2021-02-18 17:08:31 +0900

    post, put 수정

--- src/routes/todos.ts
+++ src/routes/todos.ts
@@ -32,20 +32,14 @@ export class todos {
             let sql1 = `CALL to_do_list.InsertList('${req.query.list_name}','${req.query.list_date}','${req.query.list_memo}','${req.query.list_color}')`;
 
             connection.query(sql1,function (err:Error, result):any {
-                if (err) throw err;              
-            });
-
-            let sql2 = `CALL SelectListAll()`;
-            
-            connection.query(sql2, function (err, result) {
-                if (err) throw err;
-                return res.json(result);        
+                if (err) throw err;       
+                return res.json(result);       
             });
         })
 
         router.route('/todos/:id').put((req: Request, res: Response) => {
-            console.log(req.query);
             if(req.query.status === 'Failed'){
+                console.log(req.params.id+': Failed');
                 let sql = `CALL UpdateStatusFailed(${req.params.id})`;
                 connection.query(sql, function (err: Error, result:any) {
                     if (err) throw err;
@@ -54,6 +48,7 @@ export class todos {
             }
 
             if(req.query.status === 'Done'){
+                console.log(req.params.id+': Done');
                 let sql = `CALL UpdateStatusDone(${req.params.id})`;
                 connection.query(sql, function (err: Error, result:any) {
                     if (err) throw err;
@@ -61,6 +56,7 @@ export class todos {
                 }); 
             }
             if(req.query.status === 'Edit'){
+                console.log(req.params.id+': Edit');
                 let sql= `CALL to_do_list.UpdateList('${req.params.id}','${req.query.list_name}','${req.query.list_date}','${req.query.list_memo}','${req.query.list_color}')`;
 
                 connection.query(sql, function (err: Error, result:any) {
@@ -71,6 +67,7 @@ export class todos {
         })
 
         router.route('/todos/:id').delete((req: Request, res: Response) => {
+            console.log(req.params.id+': Trash');
             let sql = `CALL UpdateTrashOff(${req.params.id})`;
             connection.query(sql, function (err: Error, result:any) {
                 if (err) throw err;
--- src/routes/trashs.ts
+++ src/routes/trashs.ts
@@ -10,6 +10,7 @@ export class trashs {
     public routes(router:Router): void {  
 
         router.route('/trashs').get((req: Request, res: Response) => {    
+            console.log("selected")
 
             let sql = `CALL SelectTrash()`;
                         
@@ -20,34 +21,21 @@ export class trashs {
         })
 
         router.route('/trashs/:id').put((req: Request, res: Response) => {
+            console.log(req.params.id+': Restore');
             let sql = `CALL UpdateTrashOn(${req.params.id})`;
             connection.query(sql, function (err: Error, result:any) {
                 if (err) throw err;
-                // console.log(result);
-                // return res.json(result);
+                return res.json(result);
             }); 
-
-            let sql2 = `CALL SelectTrash()`;
-            
-            connection.query(sql2, function (err:Error, result:any) {
-                if (err) throw err;
-                return res.json(result);        
-            });
         })
 
         router.route('/trashs/:id').delete((req: Request, res: Response) => {
+            console.log(req.params.id+': Delete');
             let sql = `CALL DeleteList(${req.params.id})`;
             connection.query(sql, function (err: Error, result:any) {
                 if (err) throw err;
-                // return res.json(result);
+                return res.json(result);
             }); 
-
-            let sql2 = `CALL SelectTrash()`;
-            
-            connection.query(sql2, function (err:Error, result:any) {
-                if (err) throw err;
-                return res.json(result);        
-            });
         })
     }
 }
